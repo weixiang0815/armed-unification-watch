a2054d9a99ff522794e26f6536caf86a015700f8
Henry Wang <henry940129@gmail.com>
2025-03-27 08:31:25 +0800

    initiate spring boot project

---
+++ backend/ArmedUnificationWatch/src/main/java/com/auw/ArmedUnificationWatchApplication.java
@@ -0,0 +1,13 @@
+package com.auw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmedUnificationWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmedUnificationWatchApplication.class, args);
+	}
+
+}
---
+++ backend/ArmedUnificationWatch/src/test/java/com/auw/ArmedUnificationWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmedUnificationWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
